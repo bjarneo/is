abc77fb67e260d1e09fb1bb0bb082eadd6c05201
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2014-11-20 20:58:38 +0100

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat');
 
 gulp.task('default', function () {
     return gulp.src('src/is.js')
+        .pipe(concat('is.min.js'))
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
         .pipe(uglify())
