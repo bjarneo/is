e356a7bff79adf94e80f8bb0f83c80a747a3c01e
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2014-11-21 20:55:27 +0100

    package updated

--- src/is.js
+++ src/is.js
@@ -4,23 +4,23 @@
 
 (function () {
     'use strict';
-    
+
     // Create our object
     var is = {};
-    
+
     // Hold our obj proto toString
     var ObjToString = Object.prototype.toString;
-    
+
     // Create our own each function, since Array forEach does not support > ie9
     function each(arr, fn) {
         var len = arr.length, i = 0;
-        
+
         if (!arr.length && arr instanceof Array) {
             throw new TypeError('Check your array.');
         } else if (typeof fn !== 'function') {
             throw new TypeError('No callback function added.');
         }
-        
+
         for (; i < len; i++) {
             fn.call(this, arr[i], i);
         }
@@ -39,21 +39,21 @@
         'Number',
         'Boolean'
     ];
-    
+
     // Add our methods
     each(methods, function (method) {
         is[method] = function (input) {
             return ObjToString.call(input) === '[object ' + method + ']';
         };
     });
-    
+
     is.Empty = function (input) {
         if (!input || (!input.length && !is.Undefined(input.length))) {
             return true;
         } else if (is.Object(input) && JSON.stringify(input) === '{}') {
             return true;
         }
-        
+
         return false;
     };
 
@@ -61,29 +61,29 @@
         if (elem && elem.nodeType === 1) {
             return true;
         }
-        
+
         return false;
     };
-    
+
     is.Integer = function (integer) {
         return parseInt(integer, 10) === integer;
     };
-    
+
     is.Float = function (number) {
         return number === +number && number !== (number|0);
     };
-    
+
     is.Global = function (input) {
         if (ObjToString.call(input) === '[object global]') {
             return true;
         }
-        
+
         return false;
     };
-    
+
     // Check if is node, amd or else we set it to window
     if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-        module.exports = is; 
+        module.exports = is;
     } else if (typeof define === 'function' && define.amd) {
         define([], function() {
                 return is;
